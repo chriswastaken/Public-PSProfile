52841a19b42ce22fc356148202a41babc48bbf21
chris <christopherpetty@gmail.com>
2017-02-10 12:37:42 -0800

    Building github based versioning.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,35 @@
-﻿
+﻿#Version 0.1
 Function Build-ChrisWasTakenProfile
 {
+ [cmdletbinding()]
  param
  (
     $CodeRoot = "$Env:SystemDrive\@Code",
     #$ProfileSource = "https://github.com/chriswastaken/Public-PSProfile"
-    $ProfileSource = "https://raw.githubusercontent.com/chriswastaken/Public-PSProfile/master/Microsoft.PowerShell_profile.ps1"
+    $ProfileSource = "https://raw.githubusercontent.com/chriswastaken/Public-PSProfile/master/Microsoft.PowerShell_profile.ps1",
+    $VerbosePreference = "Continue"
  )
+
+ ###########################
  #Profile
+ $ProfileSourceContent = (iwr $ProfileSource -UseBasicParsing).Content
  if ( ! (Test-Path $profile) )
  {
-  "Unable to find a profile @ $profile" | Write-Verbose
-  
-  
-
+  "Unable to find a profile @ $profile" | Write-Verbose 
+  $ProfileSourceContent | Out-File $Profile -Force
+  Import-Module $profile
+  "Created a profile @ $profile" | Write-Verbose 
  }
-
+ elseif ( Compare $(Get-Content $Profile) $($ProfileSourceContent) )
+ {
+  "Profile @ $profile is out of date." | Write-Verbose 
+  $ProfileSourceContent | Out-File $Profile -Force
+  Import-Module $profile
+  "Profile @ $profile updated." | Write-Verbose 
+ }
+ #EndProfile
+ ###########################
+ ###########################
  #CodeRoot
  if ( ! (Test-Path $CodeRoot) )
  {
@@ -26,11 +40,5 @@ Function Build-ChrisWasTakenProfile
  $env:Path += ";$CodeRoot"
  New-PSDrive -Name Code -PSProvider FileSystem -Root $CodeRoot | out-null
  Import-Module Code:\* -ErrorAction SilentlyContinue
- #EndCodeRoot
-
-
-
-
+ #EndCodeRoot###############
 }
-
-
